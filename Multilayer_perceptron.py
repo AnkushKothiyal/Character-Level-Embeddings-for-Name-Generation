7e1f7143f4866daa478e91b43270b6c3ae569f05
AnkushKothiyal <97513116+AnkushKothiyal@users.noreply.github.com>
2024-07-12 01:22:54 +0530

    Update Multilayer_perceptron.py

--- Multilayer_perceptron.py
+++ Multilayer_perceptron.py
@@ -139,10 +139,25 @@ logits = h2 @ W3 + b3
 test_loss = F.cross_entropy(logits, Yte)
 print("loss on test dataset = ", test_loss.item())
 
+# sample from the model
+g = torch.Generator().manual_seed(2147483647 + 10)
 
-# visualize dimensions 0 and 1 of the embedding matrix C for all characters
-plt.figure(figsize=(8,8))
-plt.scatter(C[:,0].data, C[:,1].data, s=200)
-for i in range(C.shape[0]):
-    plt.text(C[i,0].item(), C[i,1].item(), itos[i], ha="center", va="center", color='white')
-plt.grid('minor')
+for _ in range(20):
+    
+    out = []
+    context = [0] * block_size # initialize with all ...
+    while True:
+      
+      emb = C[torch.tensor([context])]    
+      h = torch.tanh(emb.view(-1, lookup_dim*block_size) @ W1 + b1)
+      h2 = torch.tanh(h @ W2 + b2)
+      logits = h2 @ W3 + b3
+      probs = F.softmax(logits, dim=1) 
+      ix = torch.multinomial(probs, num_samples=1, generator=g).item()
+      context = context[1:] + [ix]
+      out.append(ix)
+      if ix == 0:
+        break
+    
+    print(''.join(itos[i] for i in out))
+ 
